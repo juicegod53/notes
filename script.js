30981d6fb908c91a012cee0253ef200f6d22bfe2
Abhi Kalakoti <abhikalakoti@gmail.com>
2025-05-25 15:59:37 +0100

    Can edit and save notes

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function createNote() {
+function loadNotes() {
+    let jsonNotes = localStorage.getItem("notes")
+
+    if (jsonNotes) {
+        let notes = JSON.parse(jsonNotes)
+        for (let i = 0; i < notes.length; i++) {
+            createNote(null, notes[i][0], notes[i][1])
+        }
+    }
+}
+
+function createNote(e, title = "Title here", text = "Text here") {
     const note = document.createElement('div');
     note.className = 'note';
 
@@ -7,31 +18,33 @@ function createNote() {
 
     const noteTitle = document.createElement('h2');
     noteTitle.className = 'note-title';
-    noteTitle.textContent = 'Title here';
+    noteTitle.textContent = title;
+    noteTitle.setAttribute('contenteditable', 'true')
 
-    const noteButtons = document.createElement('div');
-    noteButtons.className = 'note-buttons';
+    // const noteButtons = document.createElement('div');
+    // noteButtons.className = 'note-buttons';
 
-    const editButton = document.createElement('button');
-    editButton.textContent = '📝';
+    // const editButton = document.createElement('button');
+    // editButton.textContent = '📝';
 
     const deleteButton = document.createElement('button');
     deleteButton.textContent = '❌';
-    deleteButton.addEventListener("click", function() {
-        if (confirm("Are you sure you want to delete this note?") == true) {
+    deleteButton.addEventListener('click', function() {
+        if (confirm('Are you sure you want to delete this note?') == true) {
             note.remove()
         }
     })
 
-    noteButtons.appendChild(editButton);
-    noteButtons.appendChild(deleteButton);
+    // noteButtons.appendChild(editButton);
+    // noteButtons.appendChild(deleteButton);
 
     noteTop.appendChild(noteTitle);
-    noteTop.appendChild(noteButtons);
+    noteTop.appendChild(deleteButton);
 
     const noteText = document.createElement('p');
     noteText.className = 'note-text';
-    noteText.textContent = 'Text here';
+    noteText.textContent = text;
+    noteText.setAttribute('contenteditable', 'true')
 
     note.appendChild(noteTop);
     note.appendChild(noteText);
@@ -41,5 +54,25 @@ function createNote() {
     notesContainer.insertBefore(note, lastElement)
 }
 
-document.getElementById('new').addEventListener("click", createNote)
+function saveNotes() {
+    const notesContainer = document.getElementById('notes-container')
+    let notesList = []
 
+    for (let i = 0; i < notesContainer.children.length -1; i++) {
+
+        const note = notesContainer.children[i]
+        const noteTitle = note.firstElementChild.firstElementChild.textContent
+        const noteText = note.lastElementChild.textContent
+
+        notesList.push([noteTitle, noteText])
+    }
+
+    const jsonNotes = JSON.stringify(notesList)
+    localStorage.setItem("notes", jsonNotes)
+}
+
+loadNotes()
+
+document.getElementById('new').addEventListener('click', createNote)
+
+document.getElementById('save').addEventListener('click', saveNotes)
